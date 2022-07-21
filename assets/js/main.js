72110f53eec6f274072798869dffa7be548e5109
cy.lee <box4101@nate.com>
2022-07-21 13:22:59 +0900

    base 수정사항 커밋

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,9 @@
 $(function () {
 
   var dur = 500;
-  var isOpen = true;
-  var activeName = "on";
-  var sub_height = [];
-  var max_height = 0;
-  var $headerWrap, $gnb, $gnb_li, $gnb_ul;
 
   $(document).ready(function () {
     initDOM();
-    max_height = gnb_max_height();
     bindingEvent();
   });
 
@@ -25,58 +19,14 @@ $(function () {
 
   //이벤트 바인딩
   function bindingEvent() {
-    $gnb_li.on("mouseenter", function () {
-      var $this = $(this);
-      openSub($this);
-      if (isOpen) {
-        createSubPanel();
-      }
-    });
-
-    $headerWrap.on("mouseleave", function () {
-      closeSub();
-    });
-
-    $gnb_li.on("mouseleave", function () {
-      $(this).children("a").removeClass(activeName);
-    });
+    ex();
   }
 
-  //제일 높은값의 gnb ul 값 반환하는 함수
-  function gnb_max_height() {
-    $gnb_li.each(function (index) {
-      var ul_height = $(this).find("ul").height();
-      sub_height.push(ul_height);
-      max_height = Math.max(max_height, sub_height[index]);
-    });
-    return max_height;
-  }
-
-  //서브패널 동적으로 생성후 열어주는 함수
-  function createSubPanel() {
-    $subPanel = '<div class="bgSub">';
-    $headerWrap.prepend($subPanel);
-    $subPanel = $(".bgSub");
-    $subPanel.height(max_height);
-    $subPanel.stop(true, true).slideDown(dur - 100, function () {
-      return isOpen = false;
-    });
-  }
-
-  //gnb ul 열어주는 함수
-  function openSub($this) {
-    $gnb_ul.stop(true, true).slideDown(dur);
-    $this.children("a").addClass(activeName);
-  }
-
-  //gnb_ul 닫고 sub_panel 닫은뒤 제거하는 함수
-  function closeSub() {
-    $gnb_ul.stop(true, true).slideUp();
-    $(".bgSub").stop(true, true).slideUp(dur, function () {
-      $(this).remove();
-      return isOpen = true;
-    });
+  //EX
+  function ex() {
+    console.log("123");
   }
 
 
-})();
+});
+
